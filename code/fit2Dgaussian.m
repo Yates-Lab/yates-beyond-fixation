4505805476122bc0fc21ce23e0529001800dc40b
Jake <jacoby8s@gmail.com>
2022-10-19 14:17:16 -0700

    working on unifying RF analysis

---
+++ code/fit2Dgaussian.m
@@ -0,0 +1,59 @@
+function stat = fit2Dgaussian(xx, yy, I, par0)
+% fit2Dgaussian(xx, yy, I, par0)
+% xx 
+% yy
+% par0 = initial parameter guess
+% [mux, muy, CDiag, COff, noiseFloor]
+
+X = [xx(:) yy(:)];
+
+mxI = max(I(:));
+mnI = min(I(:));
+% gaussian function
+gfun = @(params, X) params(5) + (mxI - params(5)) * exp(-.5 * sum(((X-[params(1) params(2)])*pinv([params(3) params(4); params(4) params(3)]'*[params(3) params(4); params(4) params(3)])).*(X-[params(1) params(2)]),2));
+
+% bounds
+lb = [min(xx(:)) min(yy(:)) 0 -1 mnI]; %#ok<*NASGU>
+ub = [max(xx(:)) max(yy(:)) 10 1 mnI+1];
+        
+% least-squares
+options = optimoptions('lsqcurvefit', 'Display', 'none');
+        
+try
+    phat = lsqcurvefit(gfun, par0, X, I(:), lb, ub, options);
+catch
+    phat = par0;
+end
+        
+%         try
+%             phat = lsqcurvefit(gfun, phat, X, I(:), lb, ub, options);
+%         end
+        
+%         [phat,R,~,COVB] = nlinfit(X, I(:), gfun, par0);
+%         CI = nlparci(phat, R, 'covar', COVB);
+        
+% convert paramters
+mu = phat(1:2);
+C = [phat(3) phat(4); phat(4) phat(3)]'*[phat(3) phat(4); phat(4) phat(3)];
+        
+        
+stat = struct();
+stat.gfun = gfun;
+
+% get r2
+Ihat = gfun(phat, X);
+r2 = rsquared(I(:), Ihat(:));
+stat.r2 = r2;
+
+% convert multivariate gaussian to ellipse
+trm1 = (C(1) + C(4))/2;
+trm2 = sqrt( ((C(1) - C(4))/2)^2 + C(2)^2);
+
+% half widths
+l1 =  trm1 + trm2;
+l2 = trm1 - trm2;
+
+% convert to sqrt of area to match Rosa et al., 1997
+stat.mu = mu;
+stat.C = C;
+stat.ar = sqrt(2 * l1 * l2);
--- code/get_spatial_rfs.m
+++ code/get_spatial_rfs.m
@@ -5,10 +5,10 @@ function RFs = get_spatial_rfs(Exp, varargin)
 ip = inputParser();
 ip.addParameter('ROI', [-15, -10, 15 10])
 ip.addParameter('binSize', .5)
-ip.addParameter('Frate', 60)
-ip.addParameter('win', [-5 20])
+ip.addParameter('Frate', 120)
+ip.addParameter('win', [0 20])
 ip.addParameter('eyeposExclusion', 16)
-ip.addParameter('rfthresh', 0.5)
+ip.addParameter('rfthresh', 0.7)
 ip.parse(varargin{:});
 
 dotTrials = io.getValidTrials(Exp, 'Dots');
@@ -34,9 +34,51 @@ eyePos = Exp.vpx.smo(:,2:3);
     'eyePosExclusion', eyeposexclusion * Exp.S.pixPerDeg, ...
     'eyePos', eyePos, 'frate', Frate, ...
     'fastBinning', true, ...
-    'smoothing', 2);
+    'smoothing', 1);
 
-% use indices only while eye position is on the screen
+if isempty(RobsSpace)
+    RFs = [];
+    return
+end
+
+%% use indices only there was enough eye positions
+bs = 1;
+rad = 2;
+xax = -10:bs:10;
+nx = numel(xax);
+figure(1); clf
+n = zeros(nx, nx);
+xs = nan(nx, nx);
+ys = nan(nx, nx);
+
+eyePosAtFrame = opts.eyePosAtFrame./Exp.S.pixPerDeg;
+
+for i = 1:nx
+    for j = 1:nx
+        x0 = xax(i);
+        y0 = xax(j);
+        
+        ix = find(hypot(eyePosAtFrame(:,1)-x0, eyePosAtFrame(:,2) - y0) < rad);
+        ix(ix > nt-bestlag) = [];
+        n(j,i) = numel(ix);
+        xs(j,i) = x0;
+        ys(j,i) = y0;
+    end
+end
+
+figure(1); clf;
+
+[y0,x0] = find(n==max(n(:)));
+x0 = xax(x0);
+y0 = xax(y0);
+imagesc(xs(1,:), ys(:,1)', n); hold on
+colorbar
+plot(x0, y0, 'r+')
+
+
+
+%%
+rad = 5;
 scrnBnds = (Exp.S.screenRect(3:4) - Exp.S.centerPix) / Exp.S.pixPerDeg;
 scrnBnds = 1.5 * scrnBnds;
 eyePosAtFrame = opts.eyePosAtFrame/Exp.S.pixPerDeg;
@@ -46,69 +88,141 @@ ix = (eyePosAtFrame(:,1) + ip.Results.ROI(1)) >= -scrnBnds(1) & ...
     (eyePosAtFrame(:,2) + ip.Results.ROI(2)) >= -scrnBnds(2) & ...
     (eyePosAtFrame(:,2) + ip.Results.ROI(4)) <= scrnBnds(2);
 
+ix = ix & hypot(eyePosAtFrame(:,1) - x0, eyePosAtFrame(:,2) - y0) < rad;
+
 fprintf('%02.2f%% of gaze positions are safely on screen\n', 100*mean(ix))
 
 numspikes = sum(RobsSpace(ix,:));
 
-% forward correlation
-stasFull = forwardCorrelation(full(Xstim), RobsSpace-mean(RobsSpace, 1), win, find(ix), [], true, false);
+%%
 
-% get summary statistics
-RFs = [];
-if isempty(RobsSpace)
-    return
+stasNull = simpleForcorrValid(Xstim, RobsSpace-mean(RobsSpace,1), win(2)+1, find(ix),-20);
+[stasFull,NX] = simpleForcorrValid(Xstim, RobsSpace-mean(RobsSpace,1), win(2)+1, find(ix),0);
+
+%%
+
+figure(1); clf
+NX = NX ./ max(NX);
+imagesc(reshape(NX, opts.dims))
+
+% %% forward correlation
+% stasFull = forwardCorrelation(full(Xstim), RobsSpace-mean(RobsSpace, 1), win, find(ix), [], false, false);
+% stasNull = forwardCorrelation(full(Xstim), RobsSpace-mean(RobsSpace, 1), -fliplr(win), find(ix), [], false, false);
+
+
+%%
+
+ss = squeeze(std(stasFull, [], 1));
+
+NC = numel(vis);
+sx = ceil(sqrt(NC));
+sy = round(sqrt(NC));
+figure(1); clf
+for cc = 1:NC
+    subplot(sx, sy, cc)
+    imagesc(reshape(ss(:,cc), opts.dims));
 end
+
+%% get summary statistics
+RFs = [];
+
 NC = numel(vis);
 field = 'Dots';
 sig = arrayfun(@(x) x.(field).sig, vis);
 mfr = arrayfun(@(x) x.(field).stimFr, vis);
 isiV = arrayfun(@(x) x.isiRate, vis);
-% fs_stim = round(1/median(diff(opts.frameTimes)));
-% assert(abs(fs_stim - Frate) < 1, 'get_spatial_rfs: Frame rate is wrong.')
+
 tax = 1e3*(win(1):win(2))/Frate;
 
 for cc = 1:NC
 
+    %%
+%     cc = 1;
+    %
+%     cc = cc + 1;
+    
 %     stas = stasFull(:,:,cc);
-    rfflat = stasFull(:,:,cc)./sum(Xstim)*Frate; % individual neuron STA
 
+    rfflat = stasFull(:,:,cc)./sum(Xstim)*Frate; % individual neuron STA
+    rfflatNull = stasNull(:,:,cc)./sum(Xstim)*Frate; % individual neuron STA
+    rfflat = rfflat .* NX;
+    rfflatNull = rfflatNull .* NX;
+
+    mu = reshape(mean(rfflatNull), opts.dims);
+%     mu = imgaussfilt(mu, 2);
+    
+    
+    figure(1); clf;
+    subplot(1,3,1); imagesc(reshape(rfflat(5,:)-0, opts.dims)); colorbar
+    subplot(1,3,2); imagesc(reshape(rfflat(5,:)-mu(:)', opts.dims)); colorbar
+    subplot(1,3,3); imagesc(mu); colorbar
+
+
+    figure(3); clf;
+    subplot(1,3,1); imagesc(reshape(rfflat(5,:)-0, opts.dims)); colorbar
+    subplot(1,3,2); imagesc(reshape(rfflat(5,:)-mu(:)', opts.dims)); colorbar
+    subplot(1,3,3); imagesc(reshape(std(rfflatNull), opts.dims)); colorbar
+    
+    null = rfflatNull - mu(:)'; %reshape(rfflatNull - mu(:)', [], 1);
+    
+    
+
+%     s = reshape(rfflat(5,:)-mu(:)', opts.dims);
+%     imagesc(s < ci(1) | s > ci(2)); colorbar
+
+    normrf = rfflat - mu(:)';
+
+    sd = std(rfflatNull);
+    sd = reshape(imgaussfilt(reshape(sd, opts.dims), 3), [], 1)';
+    
+%     null = null / sd;
+%     null = null(:);
+
+    ci = [-4 4];
+%     ci = prctile(null, [.1 99.9]);
+
+    figure(2); clf
+    plot(max(normrf))
+
+    normrf = normrf ./ sd;
+%     rfflatNull = rfflatNull / sd;
+    
     % reshape into 3D (space x space x time)
     nlags = numel(tax);
-    rf = reshape(rfflat, [nlags, opts.dims]); % 3D spatiotemporal tensor
+    rf = reshape(normrf, [nlags, opts.dims]); % 3D spatiotemporal tensor
     
-    % clip edges
-    rf([1 end],:,:) = 0;
-    rf(:,[1 end],:) = 0;
-    rf(:,:,[1 end]) = 0;
-
-    adiff = abs(rf - median(rf(:))); % positive difference
-    mad = median(adiff(:)); % median absolute deviation
+    hold on
+    plot(max(normrf))
+%     % clip edges
+%     rf([1 end],:,:) = 0;
+%     rf(:,[1 end],:) = 0;
+%     rf(:,:,[1 end]) = 0;
     
-    adiff = (rf - median(rf(:))) ./ mad; % normalized (units of MAD)
-    
-
-
-    % compute threshold using pre-stimulus lags
-    thresh = max(max(max(adiff(tax<=0,:))), .7*max(adiff(:)));
     
     % threshold and find the largest connected component
-    bw = bwlabeln(adiff > thresh);
-    s = regionprops3(bw);
+    sigrf = rf < ci(1) | rf > ci(2);
 
+    bw = bwlabeln(sigrf);
+    s = regionprops3(bw);
+    s(s.Volume < 2,:) = [];
+
+    sigrf = mean(sigrf(:));
+
+%%
     if isempty(s)
         disp('No Centroids. skipping.')
         % no connected components found (nothing crossed threshold)
         continue
     end
 
-    % constrain when the peak lag is viable
+    %% constrain when the peak lag is viable
     peaklagst = interp1(1:nlags, tax, s.Centroid(:,2));
     s = s(peaklagst>=0 & peaklagst <= 250,:);
 
-     if size(s,1) > 2 % no valid clusters or too many valid clusters means it's noise
-        disp('Too many centroids. skipping')
-        continue
-    end
+%      if size(s,1) > 2 % no valid clusters or too many valid clusters means it's noise
+%         disp('Too many centroids. skipping')
+%         continue
+%     end
        
     [maxV, bigg] = max(s.Volume);
 
@@ -119,7 +233,7 @@ for cc = 1:NC
     peaklagc = min(ceil(ytx(2))+1, nlags);
     peaklag = round(ytx(2));
     
-    rfflat = reshape(rf, nlags, []);
+%     rfflat = reshape(rf, nlags, []);
 
     [~, spmx] = max(rfflat(peaklag,:));
     [~, spmn] = min(rfflat(peaklag,:));    
@@ -127,22 +241,23 @@ for cc = 1:NC
     % spatial RF
     srf = squeeze(mean(rf(peaklagf:peaklagc,:,:), 1));
     
-    dt = mode(diff(opts.frameTimes));
     Stmp.rfflat = rfflat;
     Stmp.skernel = srf;
     Stmp.spower = reshape(std(rfflat), opts.dims);
     Stmp.tpower = std(rfflat, [], 2);
-    Stmp.tkernel = rfflat(:,spmx)/dt;
+    Stmp.tkernel = rfflat(:,spmx);
     Stmp.tkernelout = rfflat(:,spmn);
     Stmp.xax = opts.xax/Exp.S.pixPerDeg;
     Stmp.yax = opts.yax/Exp.S.pixPerDeg;
-    Stmp.lags = (win(1):win(2))*dt;
+    Stmp.lags = tax;
     Stmp.peaklag = peaklag;
     Stmp.peaklagt = peaklagt;
-    Stmp.sig = sig(cc);
+    Stmp.vissig = sig(cc);
+    Stmp.rfsig = sigrf;
     Stmp.mfr = mfr(cc);
     Stmp.isiV = isiV(cc);
     Stmp.numspikes = numspikes(cc);
+    Stmp.maxV = maxV;
     
     Stmp.contour = [nan nan];
     Stmp.contourConv = [nan nan];
@@ -155,22 +270,35 @@ for cc = 1:NC
 
     if peaklag == 1
         RFs = [RFs; Stmp];
-        continue
+%         continue
     end
         
     % clip edges
-    srf = srf(2:end-1,2:end-1);
+%     srf = srf(2:end-1,2:end-1);
 
-    [xx,yy] = meshgrid(opts.xax(2:end-1)/Exp.S.pixPerDeg, opts.yax(2:end-1)/Exp.S.pixPerDeg);
+%     [xx,yy] = meshgrid(opts.xax(2:end-1)/Exp.S.pixPerDeg, opts.yax(2:end-1)/Exp.S.pixPerDeg);
 %     srf = (srf - min(srf(:))) / (max(srf(:)) - min(srf(:)));
-    srf = srf ./ max(srf(:));
+%     srf = srf ./ max(srf(:));
+    [xx,yy] = meshgrid(opts.xax/Exp.S.pixPerDeg, opts.yax/Exp.S.pixPerDeg);
+%     srf = reshape(std(rfflat), opts.dims);
+    srf = Stmp.skernel;
+    if -min(srf(:)) > max(srf(:))
+        srf = -srf;
+    end
+
     
-%     sc = 4;
-%     xx = imresize(xx, sc);
-%     yy = imresize(yy, sc);
-%     srf = imresize(srf, sc);
 
     [con, ar, ctr, thresh, maxoutrf] = get_rf_contour(xx,yy,srf, 'thresh', rfthresh, 'upsample', 4);
+
+%     I = srf;% I(abs(srf) < 2) = 0;
+%     par0 = [ctr ar, 0, 0];
+%     gfit = fit2Dgaussian(xx, yy, I, par0);
+% 
+%     figure(1); clf;
+%     imagesc(xx(1,:), yy(:,1)', I)
+%     hold on
+%     plot.plotellipse(gfit.mu, gfit.C, 1, 'r', 'Linewidth', 2);
+
     
     figure(2); clf
     subplot(1,2,1)
@@ -183,21 +311,10 @@ for cc = 1:NC
    
     drawnow
     
-
-% 
-% 
-% %     mask = (hanning(opts.dims(1))*hanning(opts.dims(2))').^.25;
-% %     rf = rf .* mask;
-% 
-%     [xx,yy] = meshgrid(opts.xax/Exp.S.pixPerDeg, opts.yax/Exp.S.pixPerDeg);
-%     
-%     rf = (rf - min(rf(:))) / (max(rf(:)) - min(rf(:)));
-%     [con, ar, ctr, maxoutrf] = get_contour(xx,yy,rf, 'thresh', thresh);
-    
     if ar == 0
         RFs = [RFs; Stmp];
         % no contour found
-        continue
+%         continue
     end
     
 
